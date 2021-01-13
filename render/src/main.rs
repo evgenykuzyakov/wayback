b54995ee8eb67256eabf5b4793822403b93e7bd2
Evgeny Kuzyakov <ek@nearprotocol.com>
2021-01-13 15:55:44 -0800

    Update render

--- render/src/main.rs
+++ render/src/main.rs
@@ -21,19 +21,27 @@ fn render_board(path: &str, board: &Board) {
 
 fn main() {
     let history = load_last_history().expect("Can't load history");
-    let _ = fs::create_dir("images");
+    let _ = fs::create_dir("images_v2");
     for (index, board) in history.boards.iter().enumerate() {
-        render_board(format!("images/{:06}.png", index).as_str(), board);
+        if index > 86877 {
+            render_board(format!("images_v2/{:06}.png", index).as_str(), board);
+        }
     }
     // To join images into a video use the following command:
     //
     // ffmpeg -r 60 -i images/%06d.png -c:v libx264 -vf "fps=60,format=yuv420p" -crf 3 video_high.mp4
+    // ffmpeg -r 60 -start_number 86878 -i images_v2/%06d.png -c:v libx264 -vf "fps=60,format=yuv420p" -crf 3 video_high_v2.mp4
     //
     // Put video on top of background with scaling
     //
     // ffmpeg -loop 1 -i bg.png -i video_high.mp4 -filter_complex "overlay=679:251:shortest=1,fps=60" -c:v libx264 -crf 3  output.mp4 -y
+    // ffmpeg -loop 1 -i bg_v2.png -i video_high_v2.mp4 -filter_complex "overlay=700:180:shortest=1,fps=60" -c:v libx264 -crf 3  output_v2.mp4 -y
+    //
+    // Make audio
+    // # requires bash
+    // ffmpeg -f concat -safe 0 -i <(for f in ./*.mp3; do echo "file '$PWD/$f'"; done) -c copy output.mp3
     //
     // Join audio
     //
-    // ffmpeg -i output.mp4 -i output.mp3 -shortest -c:v copy -c:a aac -b:a 320k final.mp4
+    // ffmpeg -i output_v2.mp4 -i output_v2.mp3 -shortest -c:v copy -c:a aac -b:a 320k final_v2.mp4
 }
