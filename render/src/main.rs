8be406019cecad0549273cff55854d4f5eacb95d
Evgeny Kuzyakov <ek@nearprotocol.com>
2021-03-14 14:29:10 -0700

    V3

--- render/src/main.rs
+++ render/src/main.rs
@@ -2,7 +2,7 @@ use common::{load_last_history, Board, BOARD_HEIGHT, BOARD_WIDTH};
 use image::ImageBuffer;
 use std::fs;
 
-pub const IMG_SCALE: u32 = 12;
+pub const IMG_SCALE: u32 = 11;
 pub const IMG_WIDTH: u32 = BOARD_WIDTH * IMG_SCALE;
 pub const IMG_HEIGHT: u32 = BOARD_HEIGHT * IMG_SCALE;
 
@@ -21,21 +21,24 @@ fn render_board(path: &str, board: &Board) {
 
 fn main() {
     let history = load_last_history().expect("Can't load history");
-    let _ = fs::create_dir("images_v2");
+    let path = "content/v3/images";
+    let _ = fs::create_dir_all(path);
     for (index, board) in history.boards.iter().enumerate() {
-        if index > 86877 {
-            render_board(format!("images_v2/{:06}.png", index).as_str(), board);
+        if index > 145559 {
+            render_board(format!("{}/{:06}.png", path, index).as_str(), board);
         }
     }
     // To join images into a video use the following command:
+    // export BP="content/v3"
     //
     // ffmpeg -r 60 -i images/%06d.png -c:v libx264 -vf "fps=60,format=yuv420p" -crf 3 video_high.mp4
-    // ffmpeg -r 60 -start_number 86878 -i images_v2/%06d.png -c:v libx264 -vf "fps=60,format=yuv420p" -crf 3 video_high_v2.mp4
+    // ffmpeg -r 60 -start_number 145560 -i $BP/images/%06d.png -c:v libx264 -vf "fps=60,format=yuv420p" -crf 3 $BP/video_high.mp4
     //
     // Put video on top of background with scaling
     //
     // ffmpeg -loop 1 -i bg.png -i video_high.mp4 -filter_complex "overlay=679:251:shortest=1,fps=60" -c:v libx264 -crf 3  output.mp4 -y
-    // ffmpeg -loop 1 -i bg_v2.png -i video_high_v2.mp4 -filter_complex "overlay=700:180:shortest=1,fps=60" -c:v libx264 -crf 3  output_v2.mp4 -y
+    // ffmpeg -loop 1 -i $BP/bg.png -i $BP/video_high.mp4 -filter_complex "overlay=700:160:shortest=1,fps=60" -c:v libx264 -crf 3  $BP/output.mp4 -y
+    // ffmpeg -loop 1 -i $BP/bg.png -i $BP/video_high.mp4 -loop 1 -i $BP/fg.png -filter_complex "[0:v][1:v]overlay=700:160:shortest=1[t],[t][2:v]overlay=0:0:shortest=1,fps=60" -c:v libx264 -crf 3  $BP/output.mp4 -y
     //
     // Make audio
     // # requires bash
@@ -43,5 +46,5 @@ fn main() {
     //
     // Join audio
     //
-    // ffmpeg -i output_v2.mp4 -i output_v2.mp3 -shortest -c:v copy -c:a aac -b:a 320k final_v2.mp4
+    // ffmpeg -i $BP/output.mp4 -i $BP/output.mp3 -c:v copy -c:a aac -b:a 320k $BP/final.mp4
 }
